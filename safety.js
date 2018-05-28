012faa115d7b4419f746a4e78d4c0e89811effe2
Nick Marozick <nmarozick@gmail.com>
2018-05-27 17:13:15 -0700

    added js to safety.html

---
+++ safety.js
@@ -0,0 +1,18 @@
+//const download = document.getElementById("fileRequest"); // need to change from fileRequest
+
+//download.addEventListener("click", function(){
+	
+
+// downloadButton.addEventListener('click', function()
+// {
+// {
+    // window.location = 'waiver2012.pdf'
+    // console.log("button was clicked")
+// }
+// });
+
+function downloadButton() {
+    window.location = 'waiver2012.pdf'
+}
+
+
