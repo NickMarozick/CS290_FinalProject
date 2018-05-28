e998b63aed67f171eae9dc4980c6da9768f66c0f
Nick Marozick <nmarozick@gmail.com>
2018-05-27 20:42:21 -0700

    update fonts

--- safety.js
+++ safety.js
@@ -1,15 +1,3 @@
-//const download = document.getElementById("fileRequest"); // need to change from fileRequest
-
-//download.addEventListener("click", function(){
-	
-
-// downloadButton.addEventListener('click', function()
-// {
-// {
-    // window.location = 'waiver2012.pdf'
-    // console.log("button was clicked")
-// }
-// });
 
 function downloadButton() {
     window.location = 'waiver2012.pdf'
